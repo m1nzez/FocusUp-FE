c3a99c20a74c2844df24298f7799a4a43c9221c0
김미주 <alwn8918@gmail.com>
2024-07-16 17:48:46 +0900

    Initial Commit

---
+++ FocusUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FocusUp
+//
+//  Created by 김미주 on 16/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
