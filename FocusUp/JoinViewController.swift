e40bbdbc69ed090c53db510998611359e852e3d0
MijuKim <alwn8918@gmail.com>
2024-07-19 02:05:22 +0900

    Feat : 기본 회원가입 화면 틀 (#4)

---
+++ FocusUp/JoinViewController.swift
@@ -0,0 +1,62 @@
+//
+//  JoinViewController.swift
+//  FocusUp
+//
+//  Created by 김미주 on 19/07/2024.
+//
+
+import UIKit
+
+class JoinViewController: UIViewController {
+    // MARK: - Properties
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var joinLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmLabel: UILabel!
+    @IBOutlet weak var passwordConfirmTextField: UITextField!
+    @IBOutlet weak var joinButton: UIButton!
+    @IBOutlet weak var snsLoginLabel: UILabel!
+    
+    // MARK: - viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setAttribute()
+        setFont()
+
+    }
+    
+    // MARK: - Function
+    func setAttribute() {
+        setTextFieldAttribute(for: emailTextField)
+        setTextFieldAttribute(for: passwordTextField)
+        setTextFieldAttribute(for: passwordConfirmTextField)
+        
+        joinButton.layer.borderColor = UIColor.primary4.cgColor
+        joinButton.layer.borderWidth = 1
+        joinButton.layer.cornerRadius = 28
+    }
+    
+    func setTextFieldAttribute(for textField: UITextField) {
+        textField.layer.borderColor = UIColor.blueGray2.cgColor
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 8
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 14, height: 0))
+        textField.leftViewMode = .always
+    }
+    
+    func setFont() {
+        joinLabel.font = UIFont(name: "Pretendard-Regular", size: 18)
+        emailLabel.font = UIFont(name: "Pretendard-Regular", size: 12)
+        emailTextField.font = UIFont(name: "Pretendard-Regular", size: 16)
+        passwordLabel.font = UIFont(name: "Pretendard-Regular", size: 12)
+        passwordTextField.font = UIFont(name: "Pretendard-Regular", size: 16)
+        passwordConfirmLabel.font = UIFont(name: "Pretendard-Regular", size: 12)
+        passwordConfirmTextField.font = UIFont(name: "Pretendard-Regular", size: 16)
+        joinButton.titleLabel?.font = UIFont(name: "Pretendard-Regular", size: 18)
+        snsLoginLabel.font = UIFont(name: "Pretendard-Medium", size: 13)
+    }
+
+}
