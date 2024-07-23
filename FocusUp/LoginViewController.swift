abb4737890952af27b7ab3732f59893e8b197c6d
MijuKim <alwn8918@gmail.com>
2024-07-23 23:17:49 +0900

    Design : 소셜 로그인 버튼 추가 (#4)

--- FocusUp/LoginViewController.swift
+++ FocusUp/LoginViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
     // MARK: - Properties
-    
+    @IBOutlet weak var kakaoButton: UIButton!
+    @IBOutlet weak var naverButton: UIButton!
     
     // MARK: - viewDidLoad()
     override func viewDidLoad() {
@@ -22,10 +23,13 @@ class LoginViewController: UIViewController {
     
     // MARK: - Function
     func setAttribute() {
-        
+        kakaoButton.layer.cornerRadius = 26
+        naverButton.layer.cornerRadius = 26
     }
     
     func setFont() {
-        
+        kakaoButton.titleLabel?.font = UIFont(name: "Pretendard-Medium", size: 15)
+        kakaoButton.titleLabel?.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.85)
+        naverButton.titleLabel?.font = UIFont(name: "Pretendard-Medium", size: 15)
     }
 }
