19cff081508bcf9f488be0daf48a1f1307e3cf0a
sunhanmoo <ghdms1013@naver.com>
2024-07-24 15:32:29 +0900

    Add : BlueGray3 색상 추가 (#3)

--- FocusUp/MyPage/MyPageViewController.swift
+++ FocusUp/MyPage/MyPageViewController.swift
@@ -12,18 +12,18 @@ class MyPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.black]
+        self.navigationController?.navigationBar.titleTextAttributes = textAttributes
+        self.title = "마이페이지"
+        
     }
-    */
+
+    
 
 }
