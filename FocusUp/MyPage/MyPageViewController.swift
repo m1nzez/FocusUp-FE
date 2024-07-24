213576676734ddbaad566594003085a1f4f2cbc2
sunhanmoo <ghdms1013@naver.com>
2024-07-24 15:53:31 +0900

    Feat : 마이페이지의 설정버튼과 설정페이지 연결 (#3)

--- FocusUp/MyPage/MyPageViewController.swift
+++ FocusUp/MyPage/MyPageViewController.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
-
+    // MARK: - property
+    @IBOutlet weak var settingButton: UIBarButtonItem!
+    @IBOutlet weak var goalRoutineLabel: UILabel!
+    @IBOutlet weak var moreButton: UIButton!
+    @IBOutlet weak var addNewRoutineButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // 폰트 적용
+        self.goalRoutineLabel.font = UIFont(name: "Pretendard-Medium.otf", size: 15)
+        self.addUnderlineToMoreButton()
+        self.addNewRoutineButton.titleLabel?.font = UIFont(name: "Pretandard-Medium.otf", size: 15)
         
+        // addNewRoutineButton 테두리 설정
+        self.addNewRoutineButton.layer.cornerRadius = 8
+        self.addNewRoutineButton.layer.borderWidth = 1
+        self.addNewRoutineButton.layer.borderColor = UIColor(named: "BlueGray3")?.cgColor
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,6 +37,24 @@ class MyPageViewController: UIViewController {
         
     }
 
+    // MARK: - action
+    @IBAction func didTapSettingBtn(_ sender: Any) {
+        guard let toSettingVC = self.storyboard?.instantiateViewController(identifier: "SettingViewController") else { return }
+        self.navigationController?.pushViewController(toSettingVC, animated: true)
+    }
     
+    
+    
+    
+    private func addUnderlineToMoreButton() {
+        let title = "더보기"
+        let font = UIFont(name: "Pretandard-Regular", size: 10)
+        let attributedTitle = NSAttributedString(string: title, attributes: [
+            .font: font ?? .systemFont(ofSize: 10),
+            .underlineStyle: NSUnderlineStyle.single.rawValue,
+            .foregroundColor: UIColor.black
+        ])
+        moreButton.setAttributedTitle(attributedTitle, for: .normal)
+    }
 
 }
