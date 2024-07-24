4a8b804b16c8d0c027331700fa6edda116890f62
sunhanmoo <ghdms1013@naver.com>
2024-07-24 16:41:11 +0900

    Design : 목표루틴리스트 네비게이션바 수정 (#3)

--- FocusUp/MyPage/GoalRoutineListViewController.swift
+++ FocusUp/MyPage/GoalRoutineListViewController.swift
@@ -8,14 +8,37 @@
 import UIKit
 
 class GoalRoutineListViewController: UIViewController {
+    // MARK: - property
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        self.navigationController?.navigationBar.tintColor = UIColor.black
+        self.navigationController?.navigationBar.topItem?.title = ""
+        self.title = "목표 루틴 리스트"
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let customFont = UIFont(name: "Pretendard-Regular", size: 18) {
+            let textAttributes = [
+                NSAttributedString.Key.foregroundColor: UIColor.black,
+                NSAttributedString.Key.font: customFont
+            ]
+            self.navigationController?.navigationBar.titleTextAttributes = textAttributes
+        } else {
+            print("커스텀 폰트를 로드할 수 없습니다.")
+        }
+        
+        let backButton = UIImage(named: "arrow_left.svg")
+        let leftBarButton: UIBarButtonItem = UIBarButtonItem(image: backButton, style: .plain, target: self, action: #selector(completeButtonDidTap))
+        self.navigationItem.leftBarButtonItem = leftBarButton
+    }
     
+    // MARK: - action
+    @objc func completeButtonDidTap(_ sender: UIBarButtonItem) {
+        self.navigationController?.popViewController(animated: true)
+    }
 
 }
